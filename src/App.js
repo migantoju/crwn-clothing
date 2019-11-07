5b5c354bc41d8a7fff4db1f1bd0ec0ae93aaf703
Miguel Toledano <haunea.shop@gmail.com>
2019-11-06 18:25:21 -0600

    Broken our homepage into smaller components, finished styling our meni-item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import HomePage from './components/homepage.component';
+
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
